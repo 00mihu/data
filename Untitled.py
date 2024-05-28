ce3b7eb362464ae63853ea23a0e31724e90b50cc
mihumihu <103144701+00mihu@users.noreply.github.com>
2024-05-28 12:17:39 +0200

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+cd..
+
+
+# In[2]:
+
+
+cd/
+
+
+# In[3]:
+
+
+read_csv http://localhost:8888/edit/Downloads/OPEN%20DATA%20DK/cycledata-1.csv
+
+
+# In[4]:
+
+
+import pandas as pd
+df = pd.read_csv('Downloads/OPEN%20DATA%20DK/cycledata-1.csv')
+print(df)
+
+
+# In[14]:
+
+
+import pandas as pd
+df = pd.read_csv('\48739\Downloads\OPEN_DATA_DK\cycledata-1.csv')
+print(df)
+
+
+# In[12]:
+
+
+cd\
+
+
+# In[13]:
+
+
+cd users
+
+
+# In[16]:
+
+
+import pandas as pd
+df = pd.read_csv('48739\Downloads\OPEN_DATA_DK\cycledata-1.csv')
+print(df)
+
+
+# In[17]:
+
+
+head(df)
+
+
+# In[18]:
+
+
+df(head)
+
+
+# In[19]:
+
+
+df.head
+
+
+# In[ ]:
+
+
+df
+
